c5f214fc9518bc954454c481c9e208f6cbcb56a4
mimilasyxa <50004735+mimilasyxa@users.noreply.github.com>
2020-06-26 10:09:08 +0700

    Update 1_2.py

--- lab3/1_2.py
+++ lab3/1_2.py
@@ -2,22 +2,20 @@ import tkinter as tk
  
 window = tk.Tk()
 window.title('My Window')
-window.geometry('100x100')
+window.geometry('300x300')
 goods = {"Рыба":100}
  
 l = tk.Label(window, bg='white', width=20, text='empty')
 l.pack()
 
  
-def sum_up(event):
-	print(event)
+def sum_up(value):
+	l.text=value;
 
 var1 = tk.IntVar()
 var2 = tk.IntVar()
-c1 = tk.Checkbutton(window, text='Рыба',variable=var1, onvalue=1, offvalue=0, command=sum_up)
+c1 = tk.Checkbutton(window, text='Рыба',variable=var1, onvalue=1, offvalue=0, command=sum_up(goods['Рыба']))
 c1.pack()
-c2 = tk.Checkbutton(window, text='C++',variable=var2, onvalue=1, offvalue=0, command=sum_up)
-c2.pack()
 
 frame = tk.Frame(window, width = 100, height = 100)
 frame.bind("<Button-1>", sum_up)
